89af5fe9a7c88d7063f41238d748ee1dc927eecf
zxcq544 <zxcq544@yandex.ru>
2022-05-22 12:53:31 +0300

    Convert bad words func to class

--- bad_words_remover.py
+++ bad_words_remover.py
@@ -1,21 +1,27 @@
 import re
 
-input_string = 'негры пидарасы'
-bad_words_filename = 'bad_words.txt'
-with open(bad_words_filename, encoding='utf-8') as f:
-    bad_words = f.read().splitlines()
 
-print(f'bad words list\n{bad_words}')
-print(f'input string\n{input_string}')
+class BadWordsRemover:
+    def __init__(self, bad_words_filename='bad_words.txt'):
+        # input_string = 'негры пидарасы'
+        with open(bad_words_filename, encoding='utf-8') as f:
+            self.bad_words = f.read().splitlines()
+
+        print(f'bad words list\n{self.bad_words}')
+        # print(f'input string\n{input_string}')
+
+    def func_to_remove_bad_words(self, match):
+        input_str = match.group()
+        if input_str.lower().strip() not in self.bad_words:
+            return input_str
+        else:
+            return ''
+
+    def remove_bad_words(self, input_string):
+        result = re.sub(r'\w+\s?', self.func_to_remove_bad_words, input_string)
+        return result
 
 
-def func_to_remove_bad_words(match):
-    input_str = match.group()
-    if input_str.lower().strip() not in bad_words:
-        return input_str
-    else:
-        return ''
-
-
-result = re.sub(r'\w+\s?', func_to_remove_bad_words, input_string)
-print(f'resulting string\n{result}')
+bad_words_remover = BadWordsRemover('bad_words.txt')
+result = bad_words_remover.remove_bad_words('негры или  пидарасы')
+print(f'result string is\n{result}')
