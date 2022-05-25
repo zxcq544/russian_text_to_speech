3abb60911de21b8eea7dfde67343eb95df7a1325
zxcq544 <zxcq544@yandex.ru>
2022-05-25 11:51:09 +0300

    refactor and instructions

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import io
 import wave
 
 from fastapi import FastAPI
-from pydantic import BaseModel
 from starlette.responses import StreamingResponse
 
 from say_neural import NeuralSpeaker
@@ -11,26 +10,21 @@ neural_speaker = NeuralSpeaker()
 app = FastAPI()
 
 
-class Message(BaseModel):
-    words: str
-    speaker = 'xenia'
-
-
 @app.get("/")
 async def root(words: str, speaker='xenia', save_file=False, sample_rate=48000):
     print(f'{words}, {speaker}, {save_file} ,{sample_rate}')
-    audio_or_response = neural_speaker.speak(words=words, speaker=speaker, save_file=save_file, sample_rate=sample_rate)
+    audio_or_text = neural_speaker.speak(words=words, speaker=speaker, save_file=save_file, sample_rate=sample_rate)
     if save_file:
         f = io.BytesIO()
-        obj = wave.open(f, 'w')
-        obj.setnchannels(1)  # mono
-        obj.setsampwidth(2)
-        obj.setframerate(sample_rate)
-        obj.writeframes(audio_or_response.audio_data)
-        obj.close()
+        wav_file_in_memory = wave.open(f, 'w')
+        wav_file_in_memory.setnchannels(1)  # mono
+        wav_file_in_memory.setsampwidth(2)
+        wav_file_in_memory.setframerate(sample_rate)
+        wav_file_in_memory.writeframes(audio_or_text.audio_data)
+        wav_file_in_memory.close()
         f.seek(0)
         response = StreamingResponse(content=f, media_type="audio/wav")
         response.headers["Content-Disposition"] = f"attachment; filename = speech_audio.wav"
         return response
     else:
-        return audio_or_response
+        return audio_or_text
